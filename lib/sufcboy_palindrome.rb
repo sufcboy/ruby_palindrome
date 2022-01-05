c7c14b4c86cd1600d4431654d34082ea22ae63b4
nickbennett <nick.bennett@cutover.com>
2022-01-05 09:27:21 +0000

    Implemented integer

--- lib/sufcboy_palindrome.rb
+++ lib/sufcboy_palindrome.rb
@@ -1,15 +1,28 @@
 require 'sufcboy_palindrome/version'
 
-class String
+module SufcboyPalindrome
+
   # Returns true for a palindrome, false otherwise.
   def palindrome?
-    processed_content == processed_content.reverse
+    if processed_content.empty?
+      false 
+    else
+      processed_content == processed_content.reverse
+    end
   end
 
   private
 
   # Returns content for palindrome testing.
   def processed_content
-    scan(/[a-z]/i).join.downcase
+    to_s.scan(/[a-z0-9]/i).join.downcase
   end
+end
+
+class String
+  include SufcboyPalindrome
+end
+
+class Integer
+  include SufcboyPalindrome
 end
