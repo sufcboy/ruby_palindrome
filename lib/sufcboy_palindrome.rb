2c13e155bf604fd9d09c8a15d5f959330f39e32a
nickbennett <nick.bennett@cutover.com>
2022-01-05 09:18:31 +0000

    Finish working and refactored palindrome method

--- lib/sufcboy_palindrome.rb
+++ lib/sufcboy_palindrome.rb
@@ -1,6 +1,15 @@
-require "sufcboy_palindrome/version"
+require 'sufcboy_palindrome/version'
 
-module SufcboyPalindrome
-  class Error < StandardError; end
-  # Your code goes here...
+class String
+  # Returns true for a palindrome, false otherwise.
+  def palindrome?
+    processed_content == processed_content.reverse
+  end
+
+  private
+
+  # Returns content for palindrome testing.
+  def processed_content
+    scan(/[a-z]/i).join.downcase
+  end
 end
